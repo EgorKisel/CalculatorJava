a282072b040ea8e5722958ccae06f1cc934af88a
Egor Kisel <kisel1989@mail.ru>
2022-02-17 19:54:31 +0300

    Cделал кнопки материальными

--- app/src/main/java/com/geekbrains/mycalculatorjava/MainActivity.java
+++ app/src/main/java/com/geekbrains/mycalculatorjava/MainActivity.java
@@ -1,5 +1,7 @@
 package com.geekbrains.mycalculatorjava;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -39,6 +41,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(getAppTheme());
         setContentView(R.layout.activity_main);
 
         inputButton();
@@ -264,7 +267,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     break;
                 }
             }
+            case (R.id.SetTheme): {
+                Intent intent = new Intent(MainActivity.this, SetTheme.class);
+                startActivityForResult(intent, 123);
+
+                break;
+            }
+            default: {
+            }
 
         }
     }
+    protected void setAppTheme(int codestyle) {
+        SharedPreferences sharedPreferences = getSharedPreferences(PREF_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(PREF_THEME_KEY, codestyle);
+        editor.apply();
+    }
+
+    protected int getAppTheme() {
+        SharedPreferences sharedPreferences = getSharedPreferences(PREF_NAME, MODE_PRIVATE);
+        return sharedPreferences.getInt(PREF_THEME_KEY, R.style.Theme_HomeWorkOne);
+    }
+
 }
